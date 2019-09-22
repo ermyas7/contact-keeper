b1b44242024f6c845a9ec3d9b7886f447910dac0
ermyas7 <ermyas.fekadu@gmail.com>
2019-09-22 19:31:08 +0530

    Setup mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+
+//connect to database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
