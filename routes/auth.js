ae3f2a2dc3deb2cbf75c17bb0dcb62d70625fdfd
ermyas7 <ermyas.fekadu@gmail.com>
2019-10-07 17:43:02 +0530

    Add POST /api/auth API

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,66 @@
 const express = require('express');
 const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
+const User = require('../models/User');
 const router = express.Router();
 
 //@route POST api/auth
-//@desc Login a user
-//@access Private
+//@desc Auth user and get token
+//@access Public
 
-router.post('/', (req, res) => {
-    res.send('login user');
+router.post('/',[
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ] ,async (req, res) => {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    };
+
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg: 'Invalid credentails'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({msg: 'Invalid credentails'});
+        }
+
+        //jwt payload
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        //generate jws signiture
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        })
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 //@route GET api/auth
-//@desc Auth user and get token
+//@desc get auth user
 //@access Private
 
-router.get('/', [
-    check('username').isEmail(),
-    check('password').isLength({
-        min: 5
-    })
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errrors: errors.array()
-        })
-    }
+router.get('/' ,(req, res) => {
+    
     res.send('Auth user and get token');
 });
 
--- routes/users.js
+++ routes/users.js
@@ -19,14 +19,13 @@ router.post('/', [
 ],async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({errrors: errors.array()})
+        return res.status(400).json({errors: errors.array()})
     }
 
     const {name, email, password} = req.body;
 
     try{
         let user = await User.findOne({email});
-        
         if(user){
             return res.status(400).json({
                 msg: 'User already exists'
