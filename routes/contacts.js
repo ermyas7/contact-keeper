ea3f92e93fe460bc71c3701579117dbb3f587f9e
ermyas7 <ermyas.fekadu@gmail.com>
2019-10-09 23:01:48 +0530

    Add POST /api/contacts API

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
 
 const Contact = require('../models/Contact');
 const User = require('../models/User');
@@ -13,7 +14,7 @@ const router = express.Router();
 router.get('/',auth, async (req, res) => {
     try{
         const contacts = await Contact.find({user: req.user.id}).sort({data: -1});
-        res.send(contacts);
+        res.json(contacts);
     }catch(err){
         console.error(err.message);
         res.send('Server Error');
@@ -24,7 +25,31 @@ router.get('/',auth, async (req, res) => {
 //@desc Add new contact
 //@access Private
 
-router.post('/', (req, res) => {
+router.post('/',[auth,
+    check('name', 'Name is required').not().isEmpty()
+], async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {name, email, type, phone} = req.body;
+    try{
+        const newContact = new Contact({
+            name,
+            email,
+            type,
+            phone,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+        res.json(contact);
+
+    }catch(err){
+        console.error(err.message);
+        res.send('Server Error');  
+    }
     res.send('Add new contact');
 });
 
